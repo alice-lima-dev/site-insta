70a935a2a75de35e9dd69209ac575c549819b1ba
alice-lima-dev <125032253+alice-lima-dev@users.noreply.github.com>
2023-06-01 20:30:32 -0300

    Add files via upload
    Site ainda não terminado faltando apenas o css da pagina home e funcionamento do js

---
+++ instapics/java.js
@@ -0,0 +1,15 @@
+var telefone = document.getElementById("telefone");
+var senha = document.getElementById("senha");
+
+
+function home() {
+
+    const telefone = 152;
+    const senha = 1234
+    
+    if (telefone === 152 && senha === 1234) {
+        alert("Bem vinda, Alice ")
+    } else {
+        alert("Não")
+    }
+}
